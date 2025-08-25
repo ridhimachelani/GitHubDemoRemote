97a58629b2f6241d5ac138432b59c979a9a4d341
Ridhima Chelani <ridhima.chelani09@gmail.com>
2025-08-25 12:49:44 -0500

    change text content

--- GitHubDemo/ContentView.swift
+++ GitHubDemo/ContentView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //comments
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("This is a test")
         }
         .padding()
     }
