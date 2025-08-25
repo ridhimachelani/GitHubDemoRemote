02f85754533fade081903ec40f6d811976af94e8
Ridhima Chelani <ridhima.chelani09@gmail.com>
2025-08-25 12:55:34 -0500

    create secondview

---
+++ GitHubDemo/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  GitHubDemo
+//
+//  Created by Ridhima Chelani on 8/25/25.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondView()
+}
