e49f552c8417ba7a2fbeb137f57a7e324d3ac739
Ridhima Chelani <ridhima.chelani09@gmail.com>
2025-08-25 12:44:56 -0500

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Ridhima Chelani on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHubDemoTests/GitHubDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  GitHubDemoTests.swift
+//  GitHubDemoTests
+//
+//  Created by Ridhima Chelani on 8/25/25.
+//
+
+import Testing
+
+struct GitHubDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
